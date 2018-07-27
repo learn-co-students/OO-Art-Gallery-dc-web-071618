9ba76aae59a16fc05a01bdd5b05bcce44a9d5217
github username <github email address>
2018-07-27 08:55:04 -0400

    initial commit of lab

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,51 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def all_paintings
+    Painting.all.select { |p| p.artist == self }
+  end
+
+  def galleries
+    self.all_paintings.map { |p| p.gallery }
+  end
+
+  def cities
+    self.galleries.map { |g| g.city }
+  end
+
+  def self.exp_arr
+    all.map { |a| a.years_active }
+  end
+
+  def self.average_experience
+    self.exp_arr.inject(0) {|sum, p| sum + p}/self.exp_arr.count
+  end
 
 end
+
+
+
+# **ARTIST**
+#
+#   - Get a list of all artists
+#
+#   - Get a list of all the paintings by a specific artists
+#
+#   - Get a list of all the galleries that a specific artist has paintings in
+#
+#   - Get a list of all cities that contain galleries that a specific artist has paintings in
+#
+#   - Find the average years of experience of all artists
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,34 @@ def reload
   load 'config/environment.rb'
 end
 
+jon = Artist.new("Jon", 36)
+kate = Artist.new("Kate", 34)
+mattie = Artist.new("Matterhorn", 10)
+
+g1 = Gallery.new("NYC Arts", "New York City, NY")
+g2 = Gallery.new("Lake Side Art", "Milwaukee, WI")
+g3 = Gallery.new("Capital Creations", "Washington, DC")
+g4 = Gallery.new("Liberty Gallery", "New York City, NY")
+
+p1 = Painting.new("Apples", "impressionist")
+p1.artist = jon
+p1.gallery = g1
+
+p2 = Painting.new("Balls", "cubist")
+p2.artist = mattie
+p2.gallery = g1
+
+p3 = Painting.new("Cats", "cubist")
+p3.artist = kate
+p3.gallery = g3
+
+p4 = Painting.new("Dock Scene", "impressionist")
+p4.artist = kate
+p4.gallery = g4
+
+p5 = Painting.new("Elephant's Afternoon", "impressionist")
+p5.artist = mattie
+p5.gallery = g2
+
+
 binding.pry

8ccfa3884f273b218f6f43e5b7e85cf8b4372241
whynotski <laurawynosky@gmail.com>
2018-08-02 22:41:54 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,53 @@
+require 'pry'
 class Gallery
+#Gallery has a name and city
+#Gallery has many painting titles
+#Gallery has many artist names through painting titles
+  @@all=[]
 
   attr_reader :name, :city
 
 def initialize(name, city)
   @name = name
   @city = city
+  @@all<<self
 end
 
-
+def self.all
+  @@all
 end
+
+# **GALLERY DELIVERABLES**
+
+# Get a list of all gallery names
+def self.gallery_list
+  gallery_list = Gallery.collect do
+    |p| p.name
+  end
+gallery_list
+end
+ #Get a list of all cities that have a gallery.
+ #A city should not appear more than once in the list.
+def self.city_list
+  city_list = Gallery.collect do
+    |p| p.city
+  end
+ city_list = city_list.uniq
+end
+
+#   - Get a list of artists that have paintings at a specific gallery
+def artist_thru_gallery
+  Painting.all.select {|a| a.artist == self}
+end
+
+#   - Get a list of the names of artists that have paintings at a specific gallery
+def name_artist_thru_gallery
+  self.artist_thru_gallery.collect {|a| a.names}
+end
+#   - Get the combined years of experience of all artists at a specific gallery
+def years_active_thru_gallery
+  total_years = 0
+  active = self.artist_thru_gallery.collect {|a| a.years_active}
+  total_years = active + total_years
+end
+end# gallery Class
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,40 @@
+require 'pry'
 class Painting
+  #Painting titles belong to artists
+  #Painting titles belong to Galleries
+  #Artist Belongs to GALLERY through paintings
+  #Gallery Belongs to Artist through paintings
 
-  attr_reader :title, :style
+@@all =[]
 
-  def initialize(title, style)
+  attr_reader :title, :style, :artist, :gallery
+
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist=artist
+    @gallery=gallery
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # **PAINTING DELIVERABLES**
+def self.all_titles
+    # For each painting collect the title
+  title_list = Painting.all.collect do
+    |p| p.title
   end
 
 end
+
+def self.all_styles
+  #   - Get a list of all painting styles (a style should not appear more than once in the list)
+  style_list = Painting.all.collect do
+    |p| p.style
+  end
+  print style_list
+  end
+end # painting class
